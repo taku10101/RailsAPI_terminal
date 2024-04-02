15594079d3d6c24db2b536fa5d6a9b0c68e26261
taku10101 <taku1011ht@gmail.com>
2024-04-03 01:06:11 +0900

    add api

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :update, :destroy]
+  
+    # GET /articles
+    def index
+      @articles = Article.all
+      render json: @articles
+    end
+  
+    # GET /articles/:id
+    def show
+      render json: @article
+    end
+  
+    # POST /articles
+    def create
+      @article = Article.new(article_params)
+      if @article.save
+        render json: @article, status: :created, location: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /articles/:id
+    def update
+      if @article.update(article_params)
+        render json: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /articles/:id
+    def destroy
+      @article.destroy
+      if @article.destroyed?
+        render json: { message: 'Article was successfully deleted.' }, status: :ok
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_article
+        @article = Article.find(params[:id])
+      end
+  
+      # Only allow a list of trusted parameters through.
+      def article_params
+        params.require(:article).permit(:title, :content)
+      end
+  end
+  
